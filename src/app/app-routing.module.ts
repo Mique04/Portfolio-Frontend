295d6bdead2fbef90b7138c30a811e7c0fdf47fc
Miqueas <miqueassantillan2704@gmail.com>
2023-05-29 20:57:17 -0300

    terminando 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { LoginComponent } from './Components/login/login.component';
 import { IniciarSesionComponent } from './Components/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './Components/registrarse/registrarse.component';
+import { GuardGuard } from './Guards/guard.guard';
 
 const routes: Routes = [
-  {path:'portfolio', component: PortfolioComponent},
+  {path:'portfolio', component: PortfolioComponent, canActivate:[GuardGuard]},
   {path:'login', component: LoginComponent},
   {path:'iniciarSesion', component: IniciarSesionComponent},
   {path:'registrarse', component: RegistrarseComponent},
